9cc58ccfc39cc871221d950c109fcebba323a9ed
Mathieu Lecarme <mlecarme@bearstech.com>
2020-03-30 14:28:44 +0200

    More click. Fighting with routes.

--- web/js/modules/rooms.js
+++ web/js/modules/rooms.js
@@ -1,36 +1,79 @@
-Vue.component('room-item', {
-    props : ["name", "id"],
-    template: `<li>
-    <router-link v-bind:to="'/route' + id">{{ name }}</router-link>
-    </li>`,
+Vue.component("room-item", {
+  props: ["name", "id"],
+  template: `<li>
+    <router-link v-bind:to="'/route/' + id">{{ name }}</router-link>
+    </li>`
 });
 
-Vue.component('rooms', {
-    //props: [room],
-    template: `<div>
+Vue.component("rooms", {
+  props: [],
+  template: `<div>
         <h5>Rooms</h5>
-        <input type="text" id="room_name"/>
-<button id="room_new">New room</button>
+        <input type="text" id="room_name" v-model="room_name" placeholder="room name"/>
+        <button v-on:click="newRoom(room_name)">New room</button>
         <ul>
         <room-item v-for="room in $root.$data.rooms"
+            v-bind:id="room.room"
             v-bind:key="room.room"
             v-bind:name="room.description"
         ></room-item>
         </ul>
         </div>`,
-    data: () => {
-        return {
-            rooms: [],
-        };
+  data: () => {
+    return {
+      room_name: ""
+    };
+  },
+  methods: {
+    newRoom: function(name, event) {
+      let ab = this.$root.$data.audiobridge;
+      ab.create({
+        permanent: false,
+        record: false,
+        description: name
+      }).then(result => {
+        console.dir(result);
+        this.$root.$data.rooms.push({
+          room: result.room,
+          description: name
+        });
+        console.log("room created", result);
+        return ab.join({ room: result.room });
+      });
     }
+  }
 });
 
-Vue.component('room', {
-    template: `
+Vue.component("room", {
+  template: `
     <div>
-        <h4>{{ name }}</h4>
+        <h4>Room {{ name }}</h4>
         {{ $route.params.id }}
     </div>
     `,
-    props: ['name'],
+  data: () => {
+      return {
+          name: '',
+      }
+  },
+  mounted: function() {
+      this.data.name = $route.params.id;
+    let ab = this.$root.$data.audiobridge;
+    ab.create({ permanent: false, record: false })
+      .then(
+        result => {
+          console.log(result);
+          let l = document.location;
+          l.hash = `#${result.room}`;
+          window.history.pushState({}, "room", l);
+          return that.audiobridge.join({ room: result.room });
+        },
+        error => {
+          console.log(error);
+        }
+      )
+      .then(result => {
+        console.log(result);
+      });
+  }
 });
--- web/poc.js
+++ web/poc.js
@@ -4,6 +4,7 @@ import { audiobridge, AudioBridgeBase } from '/js/init.js';
 
 const data = {
   rooms: Array,
+  participants: Array,
 };
 
 const Home = {
@@ -15,85 +16,35 @@ const Home = {
 const routes = [
   {
     path: '/room/:id',
-    component: Vue.component('room'), 
+    component: {
+      template: `<div><room></room></div>`
+    }
   },
   {
     path: '/',
     component: Home,
   },
 ];
+
 const router = new VueRouter({
   routes // short for `routes: routes`
 });
 
 const app = new Vue({
-  el: '#app',
   data: data,
   router: router,
-});
+}).$mount('#app');
 
 class MyAudioBridge extends AudioBridgeBase {
   constructor(mixer, audio_id) {
     super(mixer, audio_id);
     let r = document.getElementById("rooms");
-    let room = window.location.hash;
     let l = document.location;
     let that = this;
-    document.getElementById("room_new").onclick = () => {
-      that.audiobridge
-        .create({
-          permanent: false,
-          record: false,
-          description: document.getElementById("room_name").value
-        })
-        .then(result => {
-          console.log("room created", result);
-          let li = document.createElement("li");
-          li.appendChild(
-            document.createTextNode(document.getElementById("room_name").value)
-          );
-          r.appendChild(li);
-          document.getElementById("room_name").value = "";
-          l.hash = `#${result.room}`;
-          window.history.pushState({}, "room", l);
-          return audiobridge.join({ room: result.room });
-        });
-    };
     this.audiobridge.list({}).then(result => {
       console.log("rooms", result);
       data.rooms = result.list;
     });
-    if (room != "") {
-      room = Number(room.substring(1));
-      if (isNaN(room)) {
-        // Room is invalid, lets reset it
-        room = "";
-        l.hash = "";
-        window.history.pushState({}, "rooms", l);
-      }
-    }
-    console.log("Hash room", room);
-    if (room == "") {
-      this.audiobridge
-        .create({ permanent: false, record: false })
-        .then(
-          result => {
-            console.log(result);
-            let l = document.location;
-            l.hash = `#${result.room}`;
-            window.history.pushState({}, "room", l);
-            return that.audiobridge.join({ room: result.room });
-          },
-          error => {
-            console.log(error);
-          }
-        )
-        .then(result => {
-          console.log(result);
-        });
-    } else {
-      this.audiobridge.join({ room: room });
-    }
   }
   onmessage(msg, jsep) {
     // We got a message/event (msg) from the plugin
@@ -160,4 +111,5 @@ audiobridge(
   ],
   MyAudioBridge, 'janus-roomaudio').then(ab => {
   console.log("audiobridge is ready: ", ab);
+  data.audiobridge = ab.audiobridge;
 });
