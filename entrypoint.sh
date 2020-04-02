cc801f6da7cada15fd21de775a05dec9a5271a04
Mathieu Lecarme <mlecarme@bearstech.com>
2020-04-02 11:43:07 +0200

    Oups the CMD.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@ PUBLIC_IP=${PUBLIC_IP:-127.0.0.1} \
     TURN_CREDENTIAL=${TURN_CREDENTIAL:passw0rd} \
     TURN_USER=${TURN_USER:janus} \
     envsubst < /etc/janus/janus.jcfg.tpl > /etc/janus/janus.jcfg
+
+exec "$@"
